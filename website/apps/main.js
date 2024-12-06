70857a5cbeb6ef2c6d9fee2d0111e7171f9abc9b
Jag Talon <jag@aangat.lahat.computer>
2024-12-05 19:50:22 -0500

    show active buttons

--- website/apps/main.js
+++ website/apps/main.js
@@ -110,6 +110,10 @@ const Search = ({apps, setSearchResults, filterGroup}) => {
       ],
     });
   }, [apps]);
+
+  const filterResults = (result) => {
+    return filterGroup === "home" ? true : result.item.category === filterGroup;
+  } 
   
   const searchFieldRef = useRef(null);
   const updateSearch = useMemo(() => {
@@ -118,9 +122,7 @@ const Search = ({apps, setSearchResults, filterGroup}) => {
         const query = searchFieldRef.current.value;
         if (query) {
           const results = fuse.search(query)
-          setSearchResults(results.filter((result) => {
-            return filterGroup === "home" ? true : result.item.category === filterGroup;
-          }));
+          setSearchResults(results.filter(filterResults));
           // console.log("search result", {results});
           return;
         }
@@ -128,9 +130,7 @@ const Search = ({apps, setSearchResults, filterGroup}) => {
       setSearchResults(
         apps
           .map((app) => ({ item: app }))
-          .filter((app) => {
-            return filterGroup === "home" ? true : app.item.category === filterGroup;
-          })
+          .filter(filterResults)
           .sort(
             (a, b) =>
               new Date(b.item.date).getTime() - new Date(a.item.date).getTime()
@@ -244,28 +244,27 @@ const MainScreen = () => {
       <a href="https://webxdc.org/docs">develop apps</a>
       <a href="https://codeberg.org/webxdc/xdcget/src/branch/main/SUBMIT.md">publish apps</a>
     </div>
-    <${Tabs} setFilterGroup=${setFilterGroup} />
+    <${Tabs} setFilterGroup=${setFilterGroup} filterGroup=${filterGroup} />
   `;
 };
 
-const Tabs = ({setFilterGroup}) => {
-
+const Tabs = ({setFilterGroup, filterGroup}) => {
   return html`<div id="tabs">
-    <button onClick=${() => { setFilterGroup("home"); }}>
+    <button onClick=${() => { setFilterGroup("home"); }} class=${filterGroup === "home" ? "active" : ""}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
         <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
         <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
       </svg>
       Home
     </button>
-    <button onClick=${() => { setFilterGroup("tool"); }}>
+    <button onClick=${() => { setFilterGroup("tool"); }} class=${filterGroup === "tool" ? "active" : ""}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
         <path d="M16.5 6a3 3 0 0 0-3-3H6a3 3 0 0 0-3 3v7.5a3 3 0 0 0 3 3v-6A4.5 4.5 0 0 1 10.5 6h6Z" />
         <path d="M18 7.5a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3h-7.5a3 3 0 0 1-3-3v-7.5a3 3 0 0 1 3-3H18Z" />
       </svg>
       Utilities
     </button>
-    <button onClick=${() => { setFilterGroup("game"); }}>
+    <button onClick=${() => { setFilterGroup("game"); }} class=${filterGroup === "game" ? "active" : ""}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
         <path d="M11.25 5.337c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.036 1.007-1.875 2.25-1.875S15 2.34 15 3.375c0 .369-.128.713-.349 1.003-.215.283-.401.604-.401.959 0 .332.278.598.61.578 1.91-.114 3.79-.342 5.632-.676a.75.75 0 0 1 .878.645 49.17 49.17 0 0 1 .376 5.452.657.657 0 0 1-.66.664c-.354 0-.675-.186-.958-.401a1.647 1.647 0 0 0-1.003-.349c-1.035 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401.31 0 .557.262.534.571a48.774 48.774 0 0 1-.595 4.845.75.75 0 0 1-.61.61c-1.82.317-3.673.533-5.555.642a.58.58 0 0 1-.611-.581c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.035-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959a.641.641 0 0 1-.658.643 49.118 49.118 0 0 1-4.708-.36.75.75 0 0 1-.645-.878c.293-1.614.504-3.257.629-4.924A.53.53 0 0 0 5.337 15c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.036 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.369 0 .713.128 1.003.349.283.215.604.401.959.401a.656.656 0 0 0 .659-.663 47.703 47.703 0 0 0-.31-4.82.75.75 0 0 1 .83-.832c1.343.155 2.703.254 4.077.294a.64.64 0 0 0 .657-.642Z" />
       </svg>
